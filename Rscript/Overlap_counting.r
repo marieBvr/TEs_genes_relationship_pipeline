4fc813bad727aff929d72b959db890ba78a9079e
CarolineMeg <carolinemeg1@gmail.com>
2020-07-28 17:10:31 +0200

    fix downstream and upstream counting

--- Rscript/Overlap_counting.r
+++ Rscript/Overlap_counting.r
@@ -1,61 +1,68 @@
 library(ggplot2)
 library(stringr)
 
+#load result file
+result_file = read.csv(file = 'ResultFile_LTR/ResultFile_LTR_Stella.tsv', sep = '\t', header = TRUE)
 
-result_file = read.csv(file = 'ResultFile_TE/ResultFile_Sibirica.tsv', sep = '\t', header = TRUE)
-count_overlap_upstream = function(result,sens){
-count = 0
+#count genes in the upstream overlap column
+count_overlap_upstream = function(result){
+count_up_plus_strand = 0
+count_down_minus_strand = 0
 for(i in 1:length(result$TE_id)){
     a = str_remove_all(result$upstream_overlap_feature[i], "[\"',\\[\\]]")
     features = strsplit(a, "\\s+")[[1]]
-    b = str_remove_all(result$upstream_overlap_strand[i], "[\"',\\[\\]]")
-    strands = strsplit(b, "\\s+")[[1]]
+    TE_strand = result$TE_strand
     if(result$upstream_overlap[i] != '[]'){
         for(j in 1:length(features)){
-            if(features[j] == 'exon' & strands[j] == sens){
-                count = count + 1
+            if(features[j] == 'exon' & TE_strand[i] == '+'){
+                count_up_plus_strand = count_up_plus_strand + 1
+                break
+                }
+            if(features[j] == 'exon' & TE_strand[i] == '-'){
+                count_down_minus_strand = count_down_minus_strand + 1
                 break
                 }
             
             }
         }
     }
-    print(count)
+    count = c(count_up_plus_strand,count_down_minus_strand)
     return(count)
 }
 
+#count genes in the downstream overlap column
 count_overlap_downstream = function(result,sens){
-count = 0
+count_up_minus_strand = 0
+count_down_plus_strand = 0
 for(i in 1:length(result$TE_id)){
     a = str_remove_all(result$downstream_overlap_feature[i], "[\"',\\[\\]]")
     features = strsplit(a, "\\s+")[[1]]
-    b = str_remove_all(result$downstream_overlap_strand[i], "[\"',\\[\\]]")
-    strands = strsplit(b, "\\s+")[[1]]
+    TE_strand = result$TE_strand
     if(result$downstream_overlap[i] != '[]'){
         for(j in 1:length(features)){
-            if(features[j] == 'exon' & strands[j] == sens){
-                count = count + 1
+            if(features[j] == 'exon' & TE_strand[i] == '+'){
+                count_down_plus_strand = count_down_plus_strand + 1
+                break
+                }
+            if(features[j] == 'exon' & TE_strand[i] == '-'){
+                count_up_minus_strand = count_up_minus_strand + 1
                 break
                 }
             
             }
         }
     }
-    print(count)
+    count = c(count_up_minus_strand,count_down_plus_strand)
     return(count)
 }
 
-c1 = count_overlap_upstream(result_file,'+')
-c2 = count_overlap_upstream(result_file,'-')
+c1 = count_overlap_upstream(result_file)
+c2 = count_overlap_downstream(result_file)
 
-c3 = count_overlap_downstream(result_file,'+')
-c4 = count_overlap_downstream(result_file,'-')
-
-count = c(c1,c2,c3,c4)
-
+#create graph
 sens = c(rep("Upstream Overlap", 2), rep("Downstream Overlap", 2))
 strand = rep(c("+","-"),2)
-value = count
+value = c(c1[1],c2[1],c1[2],c2[2])
 data = data.frame(sens,value)
 plot = ggplot(data, aes(fill=strand, y=value, x=sens )) + geom_bar(position="dodge", stat="identity") + ggtitle("Sibirica") + ylab("number of TE with an overlapping gene") + xlab("") + theme(plot.title = element_text(hjust = 0.5))
 print(plot)
